150007ba8e05c79c1caffc8e487f5f518658fbe9
hweeping-goh <hweeping.goh2@gmail.com>
2022-11-25 17:50:49 +0800

    made changes

--- scripts/gettable.js
+++ scripts/gettable.js
@@ -1,45 +1,30 @@
-const form = document.getElementById('form');
-const startdate = document.getElementById('startdate');
-const enddate = document.getElementById('enddate');
+const form = document.getElementById("form");
 
-function getDataFromApi(event) {
-  let url = "https://fe04156e-b0a5-468a-a569-dab0a548bb56.mock.pstmn.io"
+function getDataFromApi() {
+  let server = "http://localhost:3000";
 
   var settings = {
-    "url": `${url}/transactions?startdate=${startdate}&enddate=${enddate}`,
-    "method": "GET",
-    "timeout": 0,
-    "headers": {
-      "Accept": "application/json"
+    url: `${server}/users/all`,
+    method: "GET",
+    headers: {
+      Accept: "application/json",
     },
   };
-  
-  var myArray = [];
-  console.log("calling API");
 
-  $.ajax(settings).done(function (response) {
-    myArray = response;
-    buildTable(myArray);
-    console.log(myArray);
+  $.ajax(settings).done((response) => {
+    var table = document.getElementById("myTable");
+    response.forEach((record) => {
+      var row = `<tr>
+                    <td>${record.id}</td>
+                    <td>${record.name}</td>
+                    <td>${record.email}</td>
+                    <td>${record.mobile}</td>
+                    <td>${record.wallet}</td>
+                </tr>`;
+      table.innerHTML += row;
+    });
   });
   event.preventDefault();
 }
 
-
-function buildTable(data){
-  var table = document.getElementById('myTable')
-
-  for (var i = 0; i < data.length; i++){
-      var row = `<tr>
-                      <td>${data[i].date}</td>
-                      <td>${data[i].amount}</td>
-                      <td>${data[i].category}</td>
-                      <td>${data[i].description}</td>
-                      <td>${data[i].account}</td>
-                </tr>`
-      table.innerHTML += row
-
-  }
-}
-
-form.addEventListener('submit', getDataFromApi);
+form.addEventListener("submit", getDataFromApi);
