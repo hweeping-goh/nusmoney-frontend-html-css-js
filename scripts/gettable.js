0df2bae74d25ba307cd569905a3a71b0b3f98089
hweeping-goh <hweeping.goh2@gmail.com>
2022-11-28 03:33:48 +0800

    modified files

--- scripts/gettable.js
+++ scripts/gettable.js
@@ -1,40 +1,113 @@
+// const form = document.getElementById("form");
+// const startdate = document.getElementById("startdate");
+// const enddate = document.getElementById("enddate");
+
+// function getDataFromApi(event) {
+//   let url = "https://fe04156e-b0a5-468a-a569-dab0a548bb56.mock.pstmn.io";
+
+//   var settings = {
+//     url: `${url}/transactions?startdate=${startdate}&enddate=${enddate}`,
+//     method: "GET",
+//     timeout: 0,
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+
+//   var myArray = [];
+//   console.log("calling API");
+
+//   $.ajax(settings).done(function (response) {
+//     myArray = response;
+//     buildTable(myArray);
+//     console.log(myArray);
+//   });
+//   event.preventDefault();
+// }
+
+// function buildTable(data) {
+//   var table = document.getElementById("myTable");
+
+//   for (var i = 0; i < data.length; i++) {
+//     var row = `<tr>
+//                       <td>${data[i].date}</td>
+//                       <td>${data[i].amount}</td>
+//                       <td>${data[i].category}</td>
+//                       <td>${data[i].description}</td>
+//                       <td>${data[i].account}</td>
+//                 </tr>`;
+//     table.innerHTML += row;
+//   }
+// }
+
+// form.addEventListener("submit", getDataFromApi);
+
 const form = document.getElementById("form");
 const startdate = document.getElementById("startdate");
 const enddate = document.getElementById("enddate");
+const category = document.getElementById("category");
+const description = document.getElementById("description");
+
+function buildUrl() {
+  let url = `https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/transaction?`;
+  let isFirstParams = true;
+
+  if (startdate.value !== "") {
+    url += `startDate=${startdate.value}`;
+    isFirstParams = false;
+  }
+
+  if (enddate.value !== "") {
+    url += `${isFirstParams ? "" : "&"}endDate=${enddate.value}`;
+    isFirstParams = false;
+  }
+
+  if (category.value !== "") {
+    url += `${isFirstParams ? "" : "&"}category=${category.value}`;
+    isFirstParams = false;
+  }
+
+  if (description.value !== "") {
+    url += `${isFirstParams ? "" : "&"}description=${description.value}`;
+  }
+
+  return url;
+}
 
 function getDataFromApi(event) {
-  let url = "https://fe04156e-b0a5-468a-a569-dab0a548bb56.mock.pstmn.io";
-
   var settings = {
-    url: `${url}/transactions?startdate=${startdate}&enddate=${enddate}`,
+    url: buildUrl(),
     method: "GET",
     timeout: 0,
     headers: {
       Accept: "application/json",
     },
+    xhrFields: {
+      withCredentials: true,
+    },
   };
 
   var myArray = [];
-  console.log("calling API");
 
   $.ajax(settings).done(function (response) {
     myArray = response;
-    buildTable(myArray);
     console.log(myArray);
+    buildTable(myArray);
   });
   event.preventDefault();
 }
 
 function buildTable(data) {
   var table = document.getElementById("myTable");
+  table.innerHTML = "";
 
   for (var i = 0; i < data.length; i++) {
     var row = `<tr>
-                      <td>${data[i].date}</td>
-                      <td>${data[i].amount}</td>
-                      <td>${data[i].category}</td>
-                      <td>${data[i].description}</td>
-                      <td>${data[i].account}</td>
+                      <td>${data[i].Date}</td>
+                      <td>${data[i].Amount}</td>
+                      <td>${data[i].Category}</td>
+                      <td>${data[i].Description}</td>
+                      <td>${data[i].Account}</td>
                 </tr>`;
     table.innerHTML += row;
   }
--- scripts/test.js
+++ scripts/test.js
@@ -1,20 +1,57 @@
-const b = document.getElementById("getusers");
-const c = document.getElementById("getuser");
+// const b = document.getElementById("getusers");
+// const c = document.getElementById("getuser");
 
-b.addEventListener("click", async () => {
-  fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/users", {
-    method: "GET",
-    mode: "cors",
-    headers: { "Content-Type": "application/json" },
-    credentials: "include",
-  }).then((response) => response.json());
-});
+// b.addEventListener("click", async () => {
+//   fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/users", {
+//     method: "GET",
+//     mode: "cors",
+//     headers: { "Content-Type": "application/json" },
+//     credentials: "include",
+//   }).then((response) => response.json());
+// });
 
-c.addEventListener("click", async () => {
+// c.addEventListener("click", async () => {
+//   fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/user", {
+//     method: "GET",
+//     mode: "cors",
+//     headers: { "Content-Type": "application/json" },
+//     credentials: "include",
+//   }).then((response) => response.json());
+// });
+
+const getUserButton = document.getElementById("getuser");
+const addPlanButton = document.getElementById("addplan");
+
+getUserButton.addEventListener("click", async () => {
   fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/user", {
     method: "GET",
-    mode: "cors",
     headers: { "Content-Type": "application/json" },
+    mode: "cors",
     credentials: "include",
-  }).then((response) => response.json());
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data || {};
+    })
+    .catch((error) => console.log(error));
+});
+
+addPlanButton.addEventListener("click", async () => {
+  fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/Plan", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    mode: "cors",
+    credentials: "include",
+    body: JSON.stringify({
+      name: "Escape Plan DDDDD",
+      settings: '[{ "Category": "Utilities", "Value":0 }]',
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      return data || {};
+    })
+    .catch((error) => console.log(error));
 });
